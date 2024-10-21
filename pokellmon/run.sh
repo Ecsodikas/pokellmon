ad22cf35dc9bb650ee5979f0a197d8fc1e65edea
Timo Netzer <ecsodikas@gmail.com>
2024-10-22 00:19:54 +0200

    add dynamic space size to sbcl run

--- pokellmon/run.sh
+++ pokellmon/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sbcl --load "pokellmon.asd" \
+sbcl --dynamic-space-size 32000 \
+    --load "pokellmon.asd" \
     --eval "(ql:quickload :pokellmon)" \
     --eval "(in-package :pokellmon)" \
     --eval "(main)"
